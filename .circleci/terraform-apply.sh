a084a97dd4351ac7e138d54002f79dbf0812a0eb
Jeff Ashton <Jeff.Ashton@D2L.com>
2017-08-03 12:20:56 -0400

    Progress

--- .circleci/terraform-apply.sh
+++ .circleci/terraform-apply.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/terraform-init.sh
 
-echo "Generating plan to $OUTPUT_PATH"
-terraform plan -out $OUTPUT_PATH
+echo "Applying plan"
+terraform apply $PLAN_PATH
