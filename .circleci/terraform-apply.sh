15db6442fdb9ac5a835e27247ba23df7d045ab3f
Jeff Ashton <jeff.ashton@d2l.com>
2017-08-04 08:31:09 -0400

    Ensuring plan file exists

--- .circleci/terraform-apply.sh
+++ .circleci/terraform-apply.sh
@@ -4,5 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/terraform-init.sh
 
+if [[ -f "$PLAN_PATH" ]]; then
+    echo "Plan file is missing: $PLAN_PATH"
+fi
+
 echo "Applying plan"
 terraform apply $PLAN_PATH
