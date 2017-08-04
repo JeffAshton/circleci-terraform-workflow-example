f59865a9e4785586319c3d93f434449a4abe63c0
Jeff Ashton <jeff.ashton@d2l.com>
2017-08-04 08:48:50 -0400

    Fixing logic

--- .circleci/terraform-apply.sh
+++ .circleci/terraform-apply.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/terraform-init.sh
 
-if [[ -f "$PLAN_PATH" ]]; then
+if [[ ! -f "$PLAN_PATH" ]]; then
     echo "Plan file is missing: $PLAN_PATH"
     exit 1
 fi
